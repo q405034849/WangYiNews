25c5dae2fd1be22760fc18002423b951b74ed0cb
西门吹血 <405034849@qq.com>
2016-06-15 23:17:40 +0800

    添加pod框架 修改gitignore

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by 张玺科 on 16/6/15.
+//  Copyright © 2016年 张玺科. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
